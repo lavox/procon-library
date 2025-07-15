15581478218f6a0aa241e56b9df135504f6dc604
lavox <rabo@mac.com>
2025-07-15 22:22:19 +0900

    add next permutation

---
+++ test/unittest/math/NextPermutationTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NextPermutationTest {
+  @Test
+  public void test_int() {
+    int[] arr = new int[] {1, 2, 3};
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new int[] {1, 3, 2}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new int[] {2, 1, 3}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new int[] {2, 3, 1}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new int[] {3, 1, 2}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new int[] {3, 2, 1}, arr);
+    assertFalse(NextPermutation.nextPermutation(arr));
+  }
+  @Test
+  public void test_long() {
+    long[] arr = new long[] {1, 2, 3};
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new long[] {1, 3, 2}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new long[] {2, 1, 3}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new long[] {2, 3, 1}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new long[] {3, 1, 2}, arr);
+    assertTrue(NextPermutation.nextPermutation(arr));
+    assertArrayEquals(new long[] {3, 2, 1}, arr);
+    assertFalse(NextPermutation.nextPermutation(arr));
+  }
+}
